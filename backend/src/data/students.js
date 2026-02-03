652fa6bed3c822d36d8ddc502753e2008a191856
QuangCuong0211 <kamitest2005>
2026-02-03 23:44:46 +0700

    feature dang ky KTX + admin

--- backend/src/data/students.js
+++ backend/src/data/students.js
@@ -2,21 +2,21 @@ export const students = [
   {
     id: 1,
     name: "Nguyễn Văn An",
-    studentCode: "SV001",
+    mssv: "SV001",
     roomId: 1,
     gender: "Nam"
   },
   {
     id: 2,
     name: "Trần Văn Bao",
-    studentCode: "SV002",
+    mssv: "SV002",
     roomId: 1,
     gender: "Nam"
   },
   {
     id: 3,
     name: "Lê Thị Cúc",
-    studentCode: "SV003",
+    mssv: "SV003",
     roomId: 3,
     gender: "Nữ"
   }
