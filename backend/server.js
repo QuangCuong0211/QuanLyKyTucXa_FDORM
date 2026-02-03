bcdfb92c38305799d100b4e7ad599b0b1315139c
tuglam16 <nguyentunglam1216@gmail.com>
2026-02-04 01:12:58 +0700

    merge

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import areaRoutes from "./src/routes/area.route.js";
 import roomRoutes from "./src/routes/room.route.js";
@@ -14,6 +15,14 @@ app.use("/api/areas", areaRoutes);
 app.use("/api/rooms", roomRoutes);
 app.use("/api/students", studentRoutes);
 
+app.use("/uploads", express.static("uploads"));
+app.use("/registerktx", registerKTXRouter);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/ktx")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
 app.listen(3000, () => {
-  console.log("🔥 Backend running at http://localhost:3000");
+  console.log("Backend running at http://localhost:3000");
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useRoutes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 import AreaPage from "./pages/AreaPage";
 import RoomPage from "./pages/RoomPage";
 // import RoomDetailPage from "./pages/RoomDetailPage";
 import StudentPage from "./pages/StudentPage";
+import LayoutAdmin from "./layouts/admin/LayoutAdmin";
+import ClientLayout from "./layouts/ClientLayout";
+import HomeAdmin from "./pages/admin/HomeAdmin";
+import RegisterKTX from "./pages/student/RegisterKTX";
 
 function App() {
   const router = useRoutes([
