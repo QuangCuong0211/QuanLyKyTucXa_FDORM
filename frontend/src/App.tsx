0239b31e4219ef08525dcbe3c0f80898375b28c1
QuangCuong0211 <kamitest2005>
2026-02-03 22:01:57 +0700

    có navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
-import RoomPage from "./pages/RoomPage";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 import AreaPage from "./pages/AreaPage";
+import RoomPage from "./pages/RoomPage";
+// import RoomDetailPage from "./pages/RoomDetailPage";
 import StudentPage from "./pages/StudentPage";
+
 function App() {
   return (
     <>
-      <AreaPage />
-      <RoomPage />
-      <StudentPage />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/areas" />} />
+
+        <Route path="/areas" element={<AreaPage />} />
+        <Route path="/rooms" element={<RoomPage />} />
+        {/* <Route path="/rooms/:id" element={<RoomDetailPage />} /> */}
+        <Route path="/students" element={<StudentPage />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,37 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand navbar-dark bg-dark px-3">
-      <span className="navbar-brand">QL KTX</span>
-      <div className="navbar-nav">
-        <Link className="nav-link" to="/areas">Khu</Link>
-        <Link className="nav-link" to="/rooms">Phòng</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <span className="navbar-brand">QL Ký Túc Xá</span>
+
+        <div className="collapse navbar-collapse show">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <li className="nav-item">
+              <NavLink to="/areas" className="nav-link">
+                Quản lý khu
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink to="/rooms" className="nav-link">
+                Quản lý phòng
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink to="/students" className="nav-link">
+                Quản lý sinh viên
+              </NavLink>
+            </li>
+
+          </ul>
+        </div>
       </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
