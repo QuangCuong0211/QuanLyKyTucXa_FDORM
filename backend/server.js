b9d628f2fe7f610b3d2e9ef9cc45b82d4f9ea6fb
tuglam16 <nguyentunglam1216@gmail.com>
2026-02-05 13:50:27 +0700

    auto create uploads

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import fs from "fs";
 import areaRoutes from "./src/routes/area.route.js";
 import roomRoutes from "./src/routes/room.route.js";
 import studentRoutes from "./src/routes/student.route.js";
 import registerKTX from "./src/routes/registerktx.route.js";
 
 const app = express();
-
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads", { recursive: true });
+}
 app.use(cors());
 app.use(express.json());
 
